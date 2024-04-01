2204582e0fa8f9d3095f2473b104a9161b8729f2
LucasAlexSant <lucas.alexsant2@gmail.com>
2024-04-01 11:14:20 -0300

    Primeira fase: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Olá mundo");
+		Position p = new Position(1, 5);
+		
+		System.out.println(p);
 	}
 
 }

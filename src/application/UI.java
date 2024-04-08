9afa04e0e1b00419241104ea130815f985b47e3e
LucasAlexSant <lucas.alexsant2@gmail.com>
2024-04-08 10:25:03 -0300

    Pequena melhoria na impressão do tabuleiro

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,18 +1,41 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 //Classe responsável por exibir o tabuleiro do jogo de xadrez no console
 public class UI {
 
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
 	// Método estático para imprimir o tabuleiro com as peças de xadrez
 	public static void printBoard(ChessPiece[][] pieces) {
-		
+
 		// Loop para percorrer sobre as linhas do tabuleiro
 		for (int i = 0; i < pieces.length; i++) {
-			// Imprime o número da linha (de 8 a 1, pois o tabuleiro é impresso de cima para baixo)
+			// Imprime o número da linha (de 8 a 1, pois o tabuleiro é impresso de cima para
+			// baixo)
 			System.out.print((8 - i) + " ");
-			
+
 			// Loop para percorrer sobre as colunas do tabuleiro
 			for (int j = 0; j < pieces.length; j++) {
 				// Chama o método para imprimir a peça na posição atual
@@ -25,14 +48,15 @@ public class UI {
 
 	// Método privado para imprimir uma peça do tabuleiro
 	private static void printPiece(ChessPiece piece) {
-        // Verifica se a peça é nula (caso não haja peça na posição)
-        if (piece == null) {
-            System.out.print("-");
-        } else {
-            // Imprime a representação da peça
-            System.out.print(piece);
-        }
-        // Imprime um espaço após cada peça
-        System.out.print(" ");
-    }
+		if (piece == null) {
+			System.out.print("-");
+		} else {
+			if (piece.getColor() == Color.WHITE) {
+				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+			} else {
+				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+			}
+		}
+		System.out.print(" ");
+	}
 }

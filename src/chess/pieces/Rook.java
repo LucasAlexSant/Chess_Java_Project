148b75341106f59dbb9520de599801edae13fa03
LucasAlexSant <lucas.alexsant2@gmail.com>
2024-04-08 13:53:45 -0300

    Implementando movimentos possíveis da Torre

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,57 @@ public class Rook extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getCollumns()];
-		return null;
+		
+		Position p = new Position(0, 0);
+		
+		//acima
+		p.setValues(position.getRow()-1, position.getCollumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+			p.setRow(p.getRow()-1);
+		}
+		if(getBoard().positionExists(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+		
+		}
+		
+		
+		//esquerda
+		
+		p.setValues(position.getRow(), position.getCollumn() -1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+			p.setCollumn(p.getCollumn()-1);
+		}
+		if(getBoard().positionExists(p)&& isThereAnyPossibleMove()) {
+			mat[p.getRow()][p.getCollumn()] = true;
+		}
+		
+		
+		//direita
+		
+		p.setValues(position.getRow(), position.getCollumn() +1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+			p.setCollumn(p.getCollumn()+1);
+		}
+		if(getBoard().positionExists(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+		}
+		
+		
+		//baixo
+			p.setValues(position.getRow()+1, position.getCollumn());
+			while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				mat[p.getRow()][p.getCollumn()] = true;
+				p.setRow(p.getRow()+1);
+			}
+			if(getBoard().positionExists(p)) {
+				mat[p.getRow()][p.getCollumn()] = true;
+				
+			}
+		
+		
+		return mat;
 	}
 }
